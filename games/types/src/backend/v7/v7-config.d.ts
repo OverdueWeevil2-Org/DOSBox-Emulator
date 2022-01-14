2be3a14a29c663e0db226b01b89e0ba3d3f01a5c
OverdueWeevil2 <67264530+OverdueWeevil2@users.noreply.github.com>
2022-01-14 21:41:24 +0300

    Add files via upload

--- games/types/src/backend/v7/v7-config.d.ts
+++ games/types/src/backend/v7/v7-config.d.ts
@@ -1,3 +1,8 @@
 export declare const endpointBase = "https://kdhkdsv558.execute-api.eu-central-1.amazonaws.com/dev";
 export declare const personalPut: string;
 export declare const personalAcl: string;
+export declare const tokenCostGet: string;
+export declare const freeTokenGet: string;
+export declare const tokeInfoGet: string;
+export declare const startIpx: string;
+export declare const stopIox: string;
--- games/types/src/player-app.d.ts
+++ games/types/src/player-app.d.ts
@@ -1,10 +1,15 @@
 import { ClientId, DosPlayer, DosPlayerOptions } from "./player";
+import { LatencyInfo } from "./backend/jj/latency";
+export declare type SidebarPage = "main" | "latency-info" | "networking";
 export interface Props {
     player: () => DosPlayer;
     options: () => DosPlayerOptions;
     clientId: ClientId | null;
     setClientId: (clientId: ClientId | null) => void;
     requestClientId?: (userGesture: boolean) => Promise<ClientId | null>;
+    anonymousClientId: ClientId;
+    networkToken: string | null;
+    setNetworkToken: (token: string | null) => void;
     mobileControls: boolean;
     setMobileControls: (controls: boolean) => Promise<void>;
     mirroredControls: boolean;
@@ -27,6 +32,16 @@ export interface Props {
     region: string | null;
     estimatingRegion: string | null;
     setRegion: (region: string | null) => void;
+    latencyInfo: LatencyInfo | null;
+    setLatencyInfo: (latencyInfo: LatencyInfo) => void;
+    showTips: boolean;
+    setShowTips: (showTips: boolean) => void;
+    sideBarPage: SidebarPage;
+    setSideBarPage: (page: SidebarPage) => void;
+    showNewInstance: boolean;
+    setShowNewInstance: (showNewInstance: boolean) => void;
+    ipxConnected: boolean;
+    setIpxConnected: (ipxConnected: boolean) => void;
 }
 export declare function PlayerApp(playerProps: {
     player: () => DosPlayer;
